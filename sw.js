acbe6605343d066e724737cc9300f85ef0bc2cc5
sharlenex <sharlenex@users.noreply.github.com>
2026-07-18 23:39:58 +0800

    Smooth iPad merge interactions

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'sweet-merge-cafe-v7';
+const CACHE_NAME = 'sweet-merge-cafe-v8';
 const TYPES = ['coffee', 'cake', 'fruit'];
 const GENERATED_ITEM_TYPES = ['tea', 'bread', 'icecream', 'chocolate'];
 const CORE = [
