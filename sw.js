c4b4e57588bf4d72730962941087e2d2466f8175
sharlenex <sharlenex@users.noreply.github.com>
2026-07-18 22:31:56 +0800

    Refresh PWA cache for touch controls

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'sweet-merge-cafe-v5';
+const CACHE_NAME = 'sweet-merge-cafe-v6';
 const TYPES = ['coffee', 'cake', 'fruit'];
 const GENERATED_ITEM_TYPES = ['tea', 'bread', 'icecream', 'chocolate'];
 const CORE = [
